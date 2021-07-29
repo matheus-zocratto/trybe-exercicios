6a50d7f8eb01fc576a31f8c702ccb1e6f5d4cfef
matheus <matheus.zocratto@gmail.com>
2021-07-29 20:52:46 -0300

    adiçao do arquivo exercicio 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex9-d2.js
@@ -0,0 +1,9 @@
+let number = [];
+
+for(index = 1; index <= 25; index += 1){
+  number.push(index);
+}
+
+for(index2 = 0; index2 < number.length; index2 += 1){
+  console.log(number[index2] / 2);
+}
