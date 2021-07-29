8ae04ff7122b5b891d7ece4e897aca3d49d04306
matheus <matheus.zocratto@gmail.com>
2021-07-29 20:18:30 -0300

    inicio da correçao, arquivo exercicio 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex1-d2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex1-d2.js
@@ -1,11 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impar = [];
 
 for(index = 0; index < numbers.length; index += 1){
-  if(numbers[index] % 2 !== 0){
-    impar.push(numbers[index])
-    
-  }
+  console.log(numbers[index]);
+
 }
-console.log(impar);
-    
