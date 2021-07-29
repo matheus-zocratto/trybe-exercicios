4ef6eabf8ae79f207593bc95b3802ed5978c7dc8
matheus <matheus.zocratto@gmail.com>
2021-07-29 20:34:22 -0300

    correçao de commits, adicionando arquivo exercicio 6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex6-d2.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = [];
+
+for(index = 0; index < numbers.length; index +=1){
+  if(numbers[index] % 2 !== 0){
+    impares.push(numbers[index]);
+    
+  }
+}
+console.log(impares);
