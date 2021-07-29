9dbb7aa37f28a9a405deb83e473388ea720c8e39
matheus <matheus.zocratto@gmail.com>
2021-07-29 20:28:15 -0300

    correcao de commits, adiçao do arquivo exercicio 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex4-d2.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numerador = 0;
+let result = 0;
+
+for(index = 0; index < numbers.length; index +=1){
+  numerador += numbers[index]
+  result = numerador / numbers.length;
+}
+
+if(result > 20){
+  console.log ("valor maior que 20")
+}
+else if(result <= 20){
+  console.log ("valor menor ou igual a 20")
+}
