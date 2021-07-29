dde33ce4d2158a4c33325e9ad3b8224347890bdb
matheus <matheus.zocratto@gmail.com>
2021-07-29 19:55:15 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex1-d2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loopfor/ex1-d2.js
@@ -1,14 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numerador = 0
+let biggestNumber = numbers[0];
+
 for(index = 0; index < numbers.length; index +=1){
-numerador += numbers[index]
+  if(biggestNumber < numbers[index]){
+    biggestNumber = numbers[index];
+    }
 
 }
-let result = numerador / numbers.length;
-
-if(result > 20){
-  console.log("valor maior que 20");
-}
-else if(result <= 20){
-  console.log("valor menor ou igual a 20");
-}
+  console.log(biggestNumber);
+    
